eb7d7ccfccb8e297b05b099ecdd5d9f4d0d43ff4
TheOne2311 <chuquy2311@gmail.com>
2023-12-18 11:33:33 +0700

    18/12

---
+++ Assets/Scripts/HealthMana/HMValue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HMValue : MonoBehaviour
+{
+    public Image Healthbar;
+    public 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
