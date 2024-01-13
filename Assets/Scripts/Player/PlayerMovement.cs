23f670b677ded2da90e06a0f1058372a90d5de44
TheOne2311 <chuquy2311@gmail.com>
2024-01-13 20:51:54 +0700

    13/1

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,15 +3,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 3.0F;
-    private Vector3 moveDirection = Vector3.zero;
+    float moveHorizontal, moveVertical;
+    public Animator Anim;
 
-    void Update()
+    public void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        if (moveHorizontal <0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);  
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
 
+        if (moveVertical != 0 || moveHorizontal != 0)
+        {
+            Debug.Log("The Horizontal is " + moveHorizontal);
+            Debug.Log("The Vertical is" + moveVertical);
+        }
+        
         Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+        Player_Move(movement);
 
         transform.position = transform.position + movement * speed * Time.deltaTime;
     }
+
+        void Player_Move(Vector3 movement)
+    {
+        if (Anim != null)
+        {
+            if (movement.magnitude > 0)
+            {
+                Anim.SetBool("isMoving", true);
+                Anim.SetFloat("horizontal", movement.x);
+                Anim.SetFloat("vertical", movement.y);
+            }    
+            else
+            {
+                Anim.SetBool("isMoving", false);
+            }
+        }
+    }    
 }
